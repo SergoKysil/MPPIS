058bbd09623167c85b62602f8c91c6ee9d16074b
Sergo <kisluy19@ukr.net>
2020-11-05 19:27:33 +0200

    add test migration with user(test) class, add some dbcontext

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,18 @@ namespace MPPIS.Models
 {
     public class User
     {
-        
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Введіть коректний email")]
+        [Display(Name ="Email")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+
     }
 }
---
+++ Services/Config.cs
@@ -0,0 +1,8 @@
+﻿
+namespace MPPIS.Services
+{
+    public class Config
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
